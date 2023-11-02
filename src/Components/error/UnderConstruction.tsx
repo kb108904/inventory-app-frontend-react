97feec05c926b5e272123d8d0bda32459473e3bb
kb <kb108904@gmail.com>
2023-11-02 17:36:45 -0400

    add attribution of image

--- src/Components/error/UnderConstruction.tsx
+++ src/Components/error/UnderConstruction.tsx
@@ -7,6 +7,7 @@ export default function UnderConstruction() {
         src="../../src/assets/construction-image.jpg"
         alt="Under Construction"
       />
+      Image by <a href="https://www.freepik.com/free-vector/flat-construction-template_1584487.htm#query=under%20construction&position=5&from_view=keyword&track=ais">Freepik</a>
     </div>
   );
 };
