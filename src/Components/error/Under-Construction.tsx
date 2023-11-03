7fd843d27740f28d1a800b2bdf5ea2da88917de1
kb <kb108904@gmail.com>
2023-11-03 11:33:58 -0400

    fix merged mistake

--- src/Components/error/Under-Construction.tsx
+++ src/Components/error/Under-Construction.tsx
@@ -15,19 +15,3 @@ export default function UnderConstruction() {
   );
 };
 
-import React from 'react';
-import "./Under-Construction.css"
-
-export default function UnderConstruction() {
-  return (
-    <div className="under-construction">
-      <img
-        src="../../src/assets/construction-image.jpg"
-        alt="Under Construction"
-      />
-      <br/>
-      Image by <a href="https://www.freepik.com/free-vector/flat-construction-template_1584487.htm#query=under%20construction&position=5&from_view=keyword&track=ais">Freepik</a>
-    </div>
-  );
-};
-
