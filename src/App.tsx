28b9cd29e80bc44699a4c2a366c5839fa34e1984
kb <kb108904@gmail.com>
2023-11-03 14:51:10 -0400

    Add .env configurations and logic for under construction

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
 import Header from "./Components/Header";
-import Body from "./Components/Body-Section"
+import Body from "./Components/Body-Section";
 
-import Login from './Components/auth/Login'
-import Register from './Components/auth/Register'
+import Login from "./Components/auth/Login";
+import Register from "./Components/auth/Register";
 
-import { BrowserRouter as Router} from 'react-router-dom';
-import {Routes, Route, } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import UnderConstruction from "./Components/error/Under-Construction";
 
-
+const config = {
+  mode: import.meta.env.MODE,
+  under_construction: import.meta.env.VITE_APP_UNDER_CON==='true',
+};
 
 function App() {
-  return <div>
-    
-    <Router>
+  if (config.under_construction && config.mode!='dev') {
+    return (
       <UnderConstruction></UnderConstruction>
-    {/* <Header/>
+    )
+  } else {
+    return (
       <div>
-        <section>                              
-            <Routes>                                                                        
-            <Route path="/register" element={<Register/>}/>
-            <Route path="/home" element={<Body/>}/>
-            <Route path="/login" element={<Login/>}/>
-            </Routes>                    
-        </section>
-      </div> */}
-    </Router>
-    </div>
+        <Router>
+          <Header />
+          <div>
+            <section>
+              <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/home" element={<Body />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </section>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
