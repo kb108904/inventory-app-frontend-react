146df93519af8534fcbaabaecf3e4bc12b71e5f1
kb <kb108904@gmail.com>
2023-11-03 23:18:56 -0400

    format the input form

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container">
+      <div className="containerZZZ">
         <div className="footer-content">
-          <p>&copy; {new Date().getFullYear()} Your Inventory App</p>
-          <p>
-            <a href="https://www.flaticon.com/free-icons/inventory" title="Inventory icons">Icons created by muh zakaria - Flaticon</a>
+          <p>&copy; {new Date().getFullYear()} Developed by Kevin D. Brown II
+          <br/>
+          <a href="https://www.flaticon.com/free-icons/inventory" title="Inventory icons">Icons created by muh zakaria - Flaticon</a>
           </p>
         </div>
       </div>
